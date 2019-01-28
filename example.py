4283a1e59a693ebe9fc8afb906e620fec0ebbe9e
Gonzalo Franco <gonzalo_franco@gmail.com>
2019-01-28 23:10:35 +0100

    Adapted example

--- example.py
+++ example.py
@@ -21,12 +21,16 @@ def f(X):
     return res
 
 # Creating synthetic dataset
-N = 1000
+N = 100000
+N_dev = 1000
 X = np.random.rand(N, 3)
+X_dev = np.random.rand(N, 3)
 y = f(X)
+y_dev = f(X_dev)
+
 
 # Instantiating model object
 mlp = MLP(X, hidden_layers=[2,2,3], activation="relu", optimizer="adam")
 
 # Running train
-mlp.train(X,y, n_iter=10000)
+mlp.train(X,y, n_epoch=100, verbose=True)
