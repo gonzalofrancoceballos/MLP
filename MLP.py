3487dc9ec1d6e10cf657778fe762eef27a745950
Gonzalo Franco <gonzalo_franco@mckinsey.com>
2019-01-28 11:31:28 +0100

    Added first versin of batcher

--- MLP.py
+++ MLP.py
@@ -28,6 +28,64 @@ TODO:
 import numpy as np
 
 
+# Batcher
+class Batcher():
+    '''
+    Batcher class. Given a list of np.arrays of same 0-dimension, returns a 
+    a list of batches for these elements
+    '''
+    def __init__(self, data, batch_size, shuffle_on_reset=False):
+        '''
+        :param data: list containing np.arrays (type: list[np.array])
+        :param batch_size: size of each batch (type: int)
+        :param shuffle_on_reset: flag to shuffle data (type: bool)
+        '''
+        self.data = data
+        self.batch_size = batch_size
+        self.shuffle_on_reset = shuffle_on_reset
+        
+        if type(data) == list:
+            self.data_size = data[0].shape[0]
+        else:
+            self.data_size = data.shape[0]
+        self.n_batches = int(np.ceil(self.data_size/self.batch_size))
+        self.I = np.arange(0, self.data_size, dtype=int)
+        if shuffle_on_reset:
+            np.random.shuffle(self.I)
+        self.current = 0
+        
+    def shuffle(self):
+        '''
+        Re-shufle the data
+        '''
+        np.random.shuffle(self.I)
+        
+    def reset(self):
+        '''
+        Reset iteration counter
+        '''
+        if self.shuffle_on_reset:
+            self.shuffle()
+        self.current = 0
+        
+    def next(self):
+        '''
+        Get next batch
+        :return: list of np.arrays
+        '''
+        I_select = self.I[(self.current*self.batch_size):((self.current+1)*self.batch_size)]
+        batch = []
+        for elem in self.data:
+            batch.append(elem[I_select])
+        
+        if(self.current<self.n_batches-1):
+            self.current = self.current+1
+        else:
+            self.reset()
+            
+        return batch
+    
+    
 # Activation functions
 def sigmoid(x):
     """
@@ -36,7 +94,6 @@ def sigmoid(x):
     :output: result of applying sigmoid functin element-wise
     """
     return 1. / (1. + np.exp(-x))
-
 
 class Sigmoid():
     """
@@ -151,7 +208,6 @@ class Tanh():
         :return: result of the operation (type: np.array)
         """
         return 1-np.tanh(x)**2
-    
     
 class Linear():
     """
@@ -303,7 +359,6 @@ class Gradient_descent():
             layers[i].b = layers[i].b - self.learning_rate * layers[i].db
         
         return layers
-    
     
 class Adam():
     """
