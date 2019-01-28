a01ff9a6535a2107a429b7f573e28f62bb205539
Gonzalo Franco <gonzalo_franco@mckinsey.com>
2019-01-28 10:30:58 +0100

    Added swish and leaky relu activation functions Code commented

--- MLP.py
+++ MLP.py
@@ -1,18 +1,14 @@
 """
 Simple and light-weight implementation of a Multi-Layer Perceptron using Numpy
-
 Copyright 2019 Gonzalo Franco Ceballos <gonzalofrancoceballos@gmail.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
@@ -22,6 +18,10 @@ import numpy as np
 
 
 # Activation functions
+def swish(x):
+    return x*Sigmoid().forward(x)
+
+
 class Sigmoid():
     """
     Sigmoid activation function
@@ -29,16 +29,46 @@ class Sigmoid():
     
     def forward(self, x):
         """
-        Forward propagation
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return 1. / (1. + np.exp(-x))
         
     def derivate(self, x):
         """
-        Derivative of the function at one point
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return (1-self.forward(x)) * self.forward(x)
+
+class Swish():
+    """
+    Swish activation function
+    """
+    def _sigmoid(self, x):
+        return 1. / (1. + np.exp(-x))
     
+    def _sigmoid_p(self, x):
+        return (1-self._sigmoid(x)) * self._sigmoid(x)
+    
+    def forward(self, x):
+        """
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
+        """
+        return x * self._sigmoid(x)
+        
+    def derivate(self, x):
+        """
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
+        """
+        return x*self._sigmoid_p(x) + self._sigmoid(x)
+
 class Relu():
     """
     ReLu activation function
@@ -46,15 +76,46 @@ class Relu():
     
     def forward(self, x):
         """
-        Forward propagation
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return np.where(x>0, x, 0.)
     
     def derivate(self, x):
         """
-        Derivative of the function at one point
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return np.where(x>0, 1., 0.)
+
+class Leaky_relu():
+    """
+    Leaky activation function
+    """
+    def __init__(self, m=0.01):
+        """
+        Initialization of the activation function
+        :param m: slope of he function (type: float)
+        """
+        self.m = m
+        
+    def forward(self, x):
+        """
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
+        """
+        return np.where(x>0, x, self.m*x)
+    
+    def derivate(self, x):
+        """
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
+        """
+        return np.where(x>0, 1., self.m)
     
 class Tanh():
     """
@@ -63,13 +124,17 @@ class Tanh():
     
     def forward(self, x):
         """
-        Forward propagation
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return np.tanh(x)
     
     def derivate(self, x):
         """
-        Derivative of the function at one point
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return 1-np.tanh(x)**2
     
@@ -81,13 +146,17 @@ class Linear():
     
     def forward(self, x):
         """
-        Forward propagation
+        Forward propagation operation
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return x
     
     def derivate(self, x):
         """
-        Derivative of the function at one point
+        Derivative of the activation function at each point of the input tensor
+        :param x: tensor apply operation element-wise (type: np.array)
+        :return: result of the operation (type: np.array)
         """
         return 1
     
@@ -95,46 +164,86 @@ class Linear():
 # Losses
 class MSE():
     """
-    Mean Squared error class
+    Class that implements Mean Squared Error
     """
     def forward(self, actual, prediction):
+        """
+        Compute MSE error between targt and prediction
+        :param actual: target vector (type: np.array)
+        :param actual: predictions vector (type: np.array)
+        :return: vector containing element-wise MSE 
+        """
         return 0.5*((prediction-actual)**2) 
     
     def derivate(self, actual, prediction):
+        """
+        Compute the derivative of MSE error 
+        :param actual: target vector (type: np.array)
+        :param actual: predictions vector (type: np.array)
+        :return: vector containing element-wise derivative of MSE 
+        """
         return prediction - actual
     
 class Logloss():
     """
-    Mean Squared error class
+    Class that implements Logloss Error
     """
     def forward(self, actual, prediction):
+        """
+        Compute Logloss error between targt and prediction
+        :param actual: target vector (type: np.array)
+        :param actual: predictions vector (type: np.array)
+        :return: vector containing element-wise Logloss
+        """
         return actual*np.log(prediction) + (1-actual)*np.log(1-prediction)
     
     def derivate(self, actual, prediction):
+        """
+        Compute the derivative of Logloss error 
+        :param actual: target vector (type: np.array)
+        :param actual: predictions vector (type: np.array)
+        :return: vector containing element-wise derivative of Logloss
+        """
         return (actual-prediction)/(prediction*(1-actual))
     
     
 # Layers
 class Dense():
+    """
+    Class that implements a dense layer
+    Z = vector_product(X,W) + b
+    A = activation(Z)
+    where:
+    X: input matrix of shape [m,input_dim]
+    W: weights matrix of shape [input_dim, output_dim]
+    b: bias vector of size [1, output_dim]
+    """
     def __init__(self, input_dim, output_dim, activation = "sigmoid"):
         """
         Initialize layer
+        :param input_dim: input dimension of the layer (type: int)
+        :param output_dim: output dimension of the layer (type: int)
+        :param activation: activation function of the layer (type: str)
         """
     
         if activation == "sigmoid": 
             self.activation = Sigmoid()
         if activation == "relu": 
             self.activation = Relu()
+        if activation == "leaky_relu":
+            self.activation = Leaky_relu()
         if activation == "tanh": 
             self.activation = Tanh()
         if activation == "linear": 
             self.activation = Linear()
+        if activation == "swish": 
+            self.activation = Swish()
             
         self.reset_layer(input_dim, output_dim)
         
     def reset_layer(self, input_dim, output_dim):
         """
-        Randomly reset weight
+        Reset weights, bias and gradients of the layer
         """
         self.W = np.random.rand(input_dim, output_dim)
         self.b = np.random.rand(1, output_dim)
@@ -144,6 +253,10 @@ class Dense():
     def forward(self, X, update=True):
         """
         Forward pass through layer
+        :param X: input matrix to the layer (type: np.array)
+        :param update: flag to update outputs Z and A. These values need to be 
+        cached during train to compute the back-propagation pass
+        :return: result of forward operation (type: np.array)
         """
         Z = np.matmul(X, self.W) + self.b
         A = self.activation.forward(Z)
@@ -154,14 +267,23 @@ class Dense():
     
     
 # Optimizers
-class Gradient_descend():
+class Gradient_descent():
     """
-    Implements gradient descend optimizer
+    Implements gradient descent optimizer
     """
     def __init__(self, learning_rate=0.001):
+        """
+        Initialize optimizer
+        :param learning rate: learning rate of each iteration (type: float)
+        """
         self.learning_rate = learning_rate
         
     def update_weights(self, layers):
+        """
+        Perform update rule
+        :param layers: layers of the MLP to update (type: list[Dense()])
+        :return: layers with updated weights (type: list[Dense()])
+        """
         for i in range(len(layers)):
             layers[i].W = layers[i].W - self.learning_rate * layers[i].dW
             layers[i].b = layers[i].b - self.learning_rate * layers[i].db
@@ -174,6 +296,10 @@ class Adam():
     Implements Adam optimizer
     """
     def __init__(self, learning_rate=0.001):
+        """
+        Initialize optimizer
+        :param learning rate: learning rate of each iteration (type: float)
+        """
         self.learning_rate = learning_rate
         self.beta_1 = 0.9
         self.beta_2 = 0.999
@@ -182,7 +308,9 @@ class Adam():
         
     def initialize_parameters(self, layers):
         """
-        
+        Initializes momemtum and velocity parameters for each layer of MLP
+        :param layers: layers of the MLP (type: list[Dense()])
+        :return: layers with initialized parameters (type: list[Dense()])
         """
         for i, layer in enumerate(layers):
             adam = {
@@ -195,6 +323,11 @@ class Adam():
 
         
     def update_weights(self, layers):
+        """
+        Perform update rule
+        :param layers: layers of the MLP to update (type: list[Dense()])
+        :return: layers with updated weights (type: list[Dense()])
+        """
         t = self.t
         for i, layer in enumerate(layers):            
             adam  = {
@@ -238,7 +371,7 @@ class MLP():
         :param activation: activartion (type: str)
         :param loss: loss function to be used(type: str)
         :param problem: regression, binary_classification, quantile (type: str)
-        
+        :param optimizer: optimizer to use (type: str)
         """
         
         self.print_rate = 100
@@ -272,7 +405,8 @@ class MLP():
             
     def _build_architecture(self):
         """
-        Build architecture of MLP
+        Build architecture of MLP. 
+        Instantiates Dense layers inside of a list
         """
         
         self._layers = []
@@ -283,6 +417,8 @@ class MLP():
     def _forward_prop(self, X, update=True):
         """
         Computes a forward pass though the architecture of the network
+        :param X: input matrix to the network (type: np.array)
+        :return: output of the network (type: np.array)
         """
         
         A = self._layers[0].forward(X, update=update)
@@ -294,13 +430,20 @@ class MLP():
     def predict(self, X):
         """
         Computes a forward pass and returns prediction
+        Note that this operation will not update Z and A of each weight
+        :param X: input matrix to the network (type: np.array)
+        :return: output of the network (type: np.array)
         """
         pred = self._forward_prop(X, update=False)
         return pred
             
     def _back_prop(self, X, y):
         """
-        
+        Computes back-propagation pass through the network
+        It retrieves output of the final layer, self._layers[-1].A, and back-propagates
+        its error through the layers of the network, computing and updating its gradients
+        :param X: input matrix to the network (type: np.array)
+        :param y: target vector (type: np.array)
         """
         for i in np.arange(len(self._layers))+1:
             # Compute deltas deltas 
@@ -326,7 +469,13 @@ class MLP():
             
     def _train_step(self, X, y):
         """
+        Performs a complete train step of the network
+        (1) Forward pass: computes Z,A for each layer
+        (2) Back propagation: computes gradients for each layer
+        (3) Update weights: call optimizer to perform update rule
         
+        :param X: input matrix to the network (type: np.array)
+        :param y: target vector (type: np.array)
         """
         # Forward propagation
         _ = self._forward_prop(X)
@@ -340,7 +489,13 @@ class MLP():
 
     def train(self, X, y, n_iter=100, learning_rate=0.0001, reg_lambda=0.01,  verbose=True):
         """
-        
+        Run several train steps
+        :param X: input matrix to the network (type: np.array)
+        :param y: target vector (type: np.array)
+        :param n_iter: number of train iterations
+        :param learning_rate: learning rate fot the optimizer
+        :param reg_lambda: weights regulariazation factor
+        :param verbose: to plot train loss
         """
         self._optimizer.learning_rate = learning_rate
         self.reg_lambda = reg_lambda
@@ -354,9 +509,11 @@ class MLP():
     def _compute_loss(self, actual, prediction):
         """
         Computes loss between prediction and target
+        :param actual: target vector (type: np.array)
+        :param actual: predictions vector (type: np.array)
+        :return: average loss (type: float)
         """
         
         current_loss = self._loss.forward(actual, prediction)
         current_loss = np.mean(current_loss)
         return current_loss
-    
--- example.py
+++ example.py
@@ -1,7 +1,11 @@
+"""
+Snippet of code run a simple MLP model
+"""
+
 from MLP import MLP
 import numpy as np
 
-
+# Dummy function to create synthetic dataset
 def f(X):
     """
     Simple function
@@ -16,10 +20,13 @@ def f(X):
     
     return res
 
+# Creating synthetic dataset
 N = 1000
 X = np.random.rand(N, 3)
 y = f(X)
 
+# Instantiating model object
 mlp = MLP(X, hidden_layers=[2,2,3], activation="relu", optimizer="adam")
 
+# Running train
 mlp.train(X,y, n_iter=10000)
