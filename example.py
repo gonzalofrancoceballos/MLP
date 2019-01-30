Binary classification functinality -Fixed logloss function: 	1. Derivative was wrong 	2. Added epsilon for extreme values 	3. Logloss had wrong sign
-Added classification example in examples.py

example.py
@@ -1,5 +1,13 @@
 import models
 import numpy as np
+import pandas as pd
+
+"""
+Several reproducible examples for MLP model
+"""
+
+
+## REGRESSION
 
 # Dummy function to create synthetic dataset
 def f(X):
@@ -37,10 +45,32 @@ mlp.train(X,y,
           n_epoch=100,
           n_stopping_rounds=30)
 
-
 # Run predict on new data
 predictions = mlp.predict(X_pred)
 
 # Evaluate model performance using the same metric it used to train
 performance = mlp._compute_loss(predictions,y_pred)
 print(f"Prediction loss: {performance}")
+
+
+## CLASSIFICATION
+
+# Get data
+iris = pd.read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", header=None)
+feature_cols = ["v1", "v2", "v3","v4"]
+iris.columns = feature_cols +  ["class"]
+iris["target"] = np.where(iris["class"] == "Iris-setosa", 1, 0)
+X = iris[feature_cols].values
+y = iris["target"].values.reshape([-1,1])
+
+
+# Instantiating model object
+mlp = models.MLP(X, 
+                 hidden_layers=[3,3,2], 
+                 activation="swish", 
+                 optimizer="adam", 
+                 problem="binary_classification",
+                 loss = "logloss")
+
+# Model train (not usin dev this time)
+mlp.train(X,y, n_epoch=1000,learning_rate=0.01)

losses.py
@@ -46,6 +46,13 @@ class Logloss():
     """
     Class that implements Logloss Error
     """
+    def __init__(self):
+        """
+        Initialize logloss object
+        eps is a small number to avoid extreme values in predictions of 0 and 1
+        """
+        self._eps = 1e-15
+        
     def forward(self, actual, prediction):
         """
         Compute Logloss error between targt and prediction
@@ -53,7 +60,11 @@ class Logloss():
         :param actual: predictions vector (type: np.array)
         :return: vector containing element-wise Logloss
         """
-        return actual*np.log(prediction) + (1-actual)*np.log(1-prediction)
+        
+        # Clip prediction to avioid 0 and 1
+        prediction = np.clip(prediction, self._eps, 1 - self._eps)
+        
+        return -(actual*np.log(prediction) + (1-actual)*np.log(1-prediction))
     
     def derivate(self, actual, prediction):
         """
@@ -62,4 +73,8 @@ class Logloss():
         :param actual: predictions vector (type: np.array)
         :return: vector containing element-wise derivative of Logloss
         """
-        return (actual-prediction)/(prediction*(1-actual))
+        
+        # Clip prediction to avioid 0 and 1
+        prediction = np.clip(prediction, self._eps, 1 - self._eps)
+        
+        return -(actual/prediction) + ((1-actual)/(1-prediction))