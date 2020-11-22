5456533d7b30d989a825360d0f5e9ddda127d238
gonzalo_franco <gonzalo_franco@mckinsey.com>
2020-11-22 17:10:28 +0100

    Changes to use setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,53 @@
-from setuptools import setup, find_packages
-import pathlib
+"""
+This file is part of MLP project <github.com/gonzalofrancoceballos/MLP>
+Simple and light-weight implementation of a Multi-Layer Perceptron using Numpy
 
-here = pathlib.Path(__file__).parent.resolve()
+Copyright 2019 Gonzalo Franco Ceballos <gonzalofrancoceballos@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
+from setuptools import setup, find_packages
+from os import path
+from pathlib import Path
+
+
+here = Path(__file__).parent.resolve()
+name = "np_mlp"
 
 # Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+readme = (here / "README.md").read_text(encoding="utf-8")
+
+
+# get package version
+with open(path.join(here, name, "__init__.py"), encoding="utf-8") as f:
+    result = re.search(r'__version__ = ["\']([^"\']+)', f.read())
+
+    if not result:
+        raise ValueError("Can't find the version in __init__.py")
+
+    version = result.group(1)
+
+# get the dependencies and installs
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requires = [x.strip() for x in f if x.strip()]
 
 setup(
-    name="np-mlp",
-    version="0.1.0",
+    name=name,
+    version=version,
     description="Light-weight implementation of a MLP library using only Numpy",
-    long_description=long_description,  # Optional
+    long_description=readme,  # Optional
     long_description_content_type="text/markdown",  # Optional (see note above)
     url="https://github.com/gonzalofrancoceballos/MLP",
     author="Gonzalo Franco",
@@ -23,26 +60,23 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 4 -Beta",
-        # Indicate who your project is intended for
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
-        # Pick your license as you wish
         "License :: OSI Approved :: GNU General Public License (GPL)",
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate you support Python 3. These classifiers are *not*
-        # checked by 'pip install'. See instead 'python_requires' below.
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    keywords="data science, neural networks, mlp",  # Optional
+    keywords="data science, neural networks, mlp",
+
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={"": "src"},  # Optional
+    # package_dir={"": "np_mlp"},  # Optional
+
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -52,19 +86,21 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    # packages=find_packages(where="src"),  # Required
+    packages=find_packages(where=name),
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
     python_requires=">=3.5, <3.9",
+
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    # install_requires=["xxxxx"],  # Optional
+    install_requires=requires,  # Optional
+
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
