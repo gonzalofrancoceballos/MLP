575f12dd1f493e3b96388d683c6ee2338c250224
gonzalo_franco <gonzalo_franco@mckinsey.com>
2020-11-23 10:28:26 +0100

    Black applied

--- setup.py
+++ setup.py
@@ -72,11 +72,9 @@ setup(
         "Programming Language :: Python :: 3.8",
     ],
     keywords="data science, neural networks, mlp",
-
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
     # package_dir={"": "np_mlp"},  # Optional
-
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -92,7 +90,6 @@ setup(
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
     python_requires=">=3.5, <3.9",
-
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
@@ -100,7 +97,6 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=requires,  # Optional
-
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
